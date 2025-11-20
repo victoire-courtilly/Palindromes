759e478ab4d3d914afcbb33c339172d783eaa9fe
victoire-courtilly <victoire.courtilly@edu.esiee.fr>
2025-11-20 09:10:48 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+"""Module contenant une fonction vérifiant si une chaîne est un palindrome."""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """Retourne True si p est un palindrome (accents et non-alphanum retirés)."""
 
     # votre code ici
-    g = p[0:len(p)//2] 
-    d = p[(len(p)+1)//2:] 
+    p = p.lower()
+    p = p.replace("æ", "ae")
+    p = p.replace("œ", "oe")
+    intab = "àâäéèêëîïôöùûüçÿ"
+    outtab = "aaaeeeeiioouuucy"
+    trantab = p.maketrans(intab, outtab)
+    p = p.translate(trantab)
+    p = "".join(c for c in p if c.isalnum())
+
+    g = p[0:len(p)//2]
+    d = p[(len(p)+1)//2:]
     return g == d[::-1]
 
 #### Fonction principale
 
 
 def main():
-
+    """Teste quelques chaînes pour vérifier la fonction ispalindrome()."""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
